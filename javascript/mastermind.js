fdb9f0ac7972bcb6ec28a75968680f009272b090
Michal1898 <michal.volf@gmail.com>
2021-06-17 15:48:52 +0200

    select digit 0

--- javascript/mastermind.js
+++ javascript/mastermind.js
@@ -1,2 +1,4 @@
-const d = new Date();
-document.getElementById("demo").innerHTML = d.getFullYear();
+function myFunction() {
+  var x = document.getElementById("digit0").value;
+  document.getElementById("digit_zero").innerHTML = "Digit 0: " + x;
+}
