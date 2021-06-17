0a8fd45816420fe31648b15bf0e5598de4738e99
Michal1898 <michal.volf@gmail.com>
2021-06-17 17:17:38 +0200

    Button added.

--- javascript/mastermind.js
+++ javascript/mastermind.js
@@ -1,41 +1,53 @@
-let secret_code=[0,0,0,0,0]
-document.getElementById("secret").innerHTML = "Secret code: " + secret_code;
+let your_code=[0, 0, 0, 0, 0] 
+let secret_code=[1, 2, 3, 4, 5] 
+let black_stick=0 
+let white_stick=0
+
+document.getElementById("your_c").innerHTML = "Your code: " + your_code;
+document.getElementById("black_s").innerHTML = "uhadnute: " + black_stick;
+document.getElementById("white_s").innerHTML = "trefena barva: " + white_stick;
+
 function myFunction0() {
-  var x = document.getElementById("digit0").value;
-  secret_code[0]=x
-  document.getElementById("digit_zero").innerHTML = "Digit 0: " + x;
-  document.getElementById("secret").innerHTML = "Secret code: " + secret_code;
-  return x
+	var x = document.getElementById("digit0").value;
+	your_code[0]=x   
+    document.getElementById("digit_zero").innerHTML = "Digit 0: " + x;
+	document.getElementById("your_c").innerHTML = "Your code: " + your_code;
+	return x
 }
-
 function myFunction1() {
-  var x = document.getElementById("digit1").value;
-  secret_code[1]=x
-  document.getElementById("digit_one").innerHTML = "Digit 1: " + x;
-  document.getElementById("secret").innerHTML = "Secret code: " + secret_code;
-  return x
+	var x = document.getElementById("digit1").value;
+	your_code[1]=x   
+    document.getElementById("digit_one").innerHTML = "Digit 1: " + x;
+	document.getElementById("your_c").innerHTML = "Your code: " + your_code;
+	return x
 }
-
 function myFunction2() {
-  var x = document.getElementById("digit2").value;
-  secret_code[2]=x
-  document.getElementById("digit_two").innerHTML = "Digit 2: " + x;
-  document.getElementById("secret").innerHTML = "Secret code: " + secret_code;
-  return x
+	var x = document.getElementById("digit2").value;
+	your_code[2]=x   
+    document.getElementById("digit_two").innerHTML = "Digit 2: " + x;
+	document.getElementById("your_c").innerHTML = "Your code: " + your_code;
+	return x
 }
-
 function myFunction3() {
-  var x = document.getElementById("digit3").value;
-  secret_code[3]=x
-  document.getElementById("digit_three").innerHTML = "Digit 3: " + x;
-  document.getElementById("secret").innerHTML = "Secret code: " + secret_code;
-  return x
+	var x = document.getElementById("digit3").value;
+	your_code[3]=x   
+    document.getElementById("digit_three").innerHTML = "Digit 3: " + x;
+	document.getElementById("your_c").innerHTML = "Your code: " + your_code;
+	return x
+}
+function myFunction4() {
+	var x = document.getElementById("digit4").value;
+	your_code[4]=x   
+    document.getElementById("digit_four").innerHTML = "Digit 4: " + x;
+	document.getElementById("your_c").innerHTML = "Your code: " + your_code;
+	return x
 }
 
-function myFunction4() {
-  var x = document.getElementById("digit4").value;
-  secret_code[4]=x
-  document.getElementById("digit_four").innerHTML = "Digit 4: " + x;
-  document.getElementById("secret").innerHTML = "Secret code: " + secret_code;
-  return x
+function code_compare() {
+	document.getElementById("secret_c").innerHTML = "Secret code: " + secret_code;
+	black_stick=1      
+    white_stick=2      
+    document.getElementById("black_s").innerHTML = "uhadnute: " + black_stick;
+	document.getElementById("white_s").innerHTML = "trefena barva: " + white_stick;
+	return white_stick
 }
