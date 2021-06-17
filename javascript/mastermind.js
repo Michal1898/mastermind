137f145d88f9cfeac33fc2c52925ba1cbba19489
Michal1898 <michal.volf@gmail.com>
2021-06-17 15:59:50 +0200

    digit 0-4 added.

--- javascript/mastermind.js
+++ javascript/mastermind.js
@@ -1,4 +1,29 @@
-function myFunction() {
+function myFunction0() {
   var x = document.getElementById("digit0").value;
   document.getElementById("digit_zero").innerHTML = "Digit 0: " + x;
+  return x
+}
+
+function myFunction1() {
+  var x = document.getElementById("digit1").value;
+  document.getElementById("digit_one").innerHTML = "Digit 1: " + x;
+  return x
+}
+
+function myFunction2() {
+  var x = document.getElementById("digit2").value;
+  document.getElementById("digit_two").innerHTML = "Digit 2: " + x;
+  return x
+}
+
+function myFunction3() {
+  var x = document.getElementById("digit3").value;
+  document.getElementById("digit_three").innerHTML = "Digit 3: " + x;
+  return x
+}
+
+function myFunction4() {
+  var x = document.getElementById("digit4").value;
+  document.getElementById("digit_four").innerHTML = "Digit 4: " + x;
+  return x
 }
